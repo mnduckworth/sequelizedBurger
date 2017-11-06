d17c80a7a5819940f42c9ed8651181f71c69fbc0
Matthew Duckworth <matthewduckworth100@gmail.com>
2017-11-05 18:49:59 -0600

    this is dumb

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var burger = require('../models')['burgers'];
+
+router.get('/', function(req, res) {
+    res.redirect('/burgers')
+});
+
+
+router.get('/burgers', function(req, res) {
+    burger.findAll({}).then(function(data) {
+        var hbsObject = { burgers: data }
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/burgers/create', function(req, res) {
+    burger.create({ burger_name: req.body.burger_name }, { devoured: req.body.devoured }).then(function(data) {
+        res.redirect('/burgers')
+    })
+});
+
+router.put('/burgers/update/:id', function(req, res) {
+    burger.update({ devoured: req.body.devoured }, {
+        fields: ['devoured'],
+        where: { id: req.params.id }
+    }).then(function(data) {
+    	res.redirect('/burgers')
+    });
+});
+
+module.exports = router;
